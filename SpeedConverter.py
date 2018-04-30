00fcd4370d65ff3e51e4b74b5afcb2db122c404e
Shoanar <shoanar@zoho.com>
2018-04-30 12:11:46 +1200

    more

--- SpeedConverter.py
+++ SpeedConverter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#Converts kmh speed to mph. Add more comments
+#Converts kmh speed to mph. Add more comments. and more
 kmh = input("Enter km/h: ")
 mph =  0.6214 * float(kmh)
 print ("Speed:", kmh, "KM/H = ", mph, "MPH")
