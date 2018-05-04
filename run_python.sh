5f962bc95e5517d997df160bd7a4df6f43203ada
Shoanar <shoanar@zoho.com>
2018-05-04 15:32:37 +1200

    Shell script more fixes

--- run_python.sh
+++ run_python.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/python
-mklink "C:\Program Files\Git\bin\nohup.exe" "C:\Program Files\git\usr\bin\nohup.exe"
-mklink "C:\Program Files\Git\bin\msys-2.0.dll" "C:\Program Files\git\usr\bin\msys-2.0.dll"
-mklink "C:\Program Files\Git\bin\msys-iconv-2.dll" "C:\Program Files\git\usr\bin\msys-iconv-2.dll"
-mklink "C:\Program Files\Git\bin\msys-intl-8.dll" "C:\Program Files\git\usr\bin\msys-intl-8.dll"
 chmod a+x fibo.py
+chmod a+x SpeedConverter.py
 python fibo.py 
 python SpeedConverter.py
 #comment
